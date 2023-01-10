d2613dfdcb789ea66065c4bf147bc3ac57a7d26f
Yuuki Arisawa <yuuki.ari@gmail.com>
2023-01-10 18:28:24 +0000

    Enable eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	 "plugins": [
+        "html",
+        "html-erb"
+  ],
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
